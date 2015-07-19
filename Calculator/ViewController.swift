f03bc346d6389511da4f1d5d6a3656912820976d
appacmp <appacmp@appacmps-MacBook-Pro.local>
2015-07-19 11:57:12 +0200

    fisrt commit

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,16 +10,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    @IBOutlet weak var display: UILabel!
+
+    var numba: Double = 0.0
+    var operation: String = "."
+    
+    
+    var displayValue: Double {
+        get {
+            return (display.text! as NSString).doubleValue
+        }
+        
+        set{
+            display.text = "\(newValue)"
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func pushDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        
+        display.text = display.text == "0" ? digit : display.text! + digit
     }
-
-
+    
+    @IBAction func pushOperator(sender: UIButton) {
+        operation = sender.currentTitle!
+        numba = displayValue
+        display.text = "0"
+        
+    }
+    
+    @IBAction func compute() {
+        switch operation {
+        case "+": displayValue = displayValue + numba
+        case "-": displayValue = -displayValue + numba
+        case "*": displayValue = displayValue * numba
+        case "/": displayValue = numba/displayValue
+        default: displayValue = 0.0
+            
+        }
+    }
+    @IBAction func clear(sender: AnyObject) {
+        display.text = "0"
+        
+    }
 }
 
